6a03c4256137890fdbd9d981d393512835740812
Arunakgit <48655744+Arunakgit@users.noreply.github.com>
2022-02-13 12:20:39 +0530

    updated 2

--- script.sh
+++ script.sh
@@ -1,4 +1,6 @@
-#/bin/sh
-echo "\n--------- Disk output -------------\n "
-df -h
-
+#!/bin/bash
+while true; do
+    echo "\n--------- Disk output -------------\n 
+    df -h >> /root/cron-out/df.log
+    sleep 300
+done
