40aa93fe443c0d10c42cb857ad7d10655c300ac4
Arunakgit <48655744+Arunakgit@users.noreply.github.com>
2022-02-13 12:26:08 +0530

    modified

--- script.sh
+++ script.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 while true; do
-    echo "\n--------- Disk output -------------\n 
-    df -h >> /root/cron-out/df.log
-    date 
+    df -h ; date >> /root/cron-out/df.log
     sleep 300
 done
